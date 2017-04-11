f35ba58ff83c8c33d3c9864f40c0779b9fcf433c
vikkylix <nikerova94@gmail.com>
2017-04-11 20:21:25 +0400

    *

--- ds.cpp
+++ ds.cpp
@@ -107,6 +107,9 @@ DS::DS(QWidget *parent) :
            QTableWidgetItem *it = new QTableWidgetItem();
            it->setText(QString("%1").arg(DS.at(i)));
            ui->tableWidget->setItem(0,i, it);
+           if (DS.at(i)<0) ui->tableWidget->item(0,i)->setBackgroundColor(Qt::red);
+           else if(DS.at(i)<0.3) ui->tableWidget->item(0,i)->setBackgroundColor(Qt::yellow);
+           else if (DS.at(i)>=0.3) ui->tableWidget->item(0,i)->setBackgroundColor(Qt::green);
 //           ui->tableWidget->itemAt(i,0)->setText(DS.at(i));
        }
        chartBuild(DS);
